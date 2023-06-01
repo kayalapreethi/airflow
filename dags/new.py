cbd095b1ae7f063aa7172f54ffbf110fa9e96d75
kayalapreethi <kpreethi@altimetrik.com>
2023-06-01 11:11:06 +0530

    airflow files

--- dags/new.py
+++ dags/new.py
@@ -3,41 +3,33 @@ from datetime import datetime
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-args = {
-    'owner': airflow,
-    'start_date': datetime(2023, 5, 30)
-}
-dag_psql = DAG(
-    dag_id="new_dag",
-    default_args=args,
-    schedule_interval='0 11 * * *',
-    description='use case of psql operator in airflow',
-    start_date=datetime(2023, 5, 30)
-)
-
-emp_details = PostgresOperator(
-    task_id='emp_details',
-    sql='''CREATE TABLE new_emp(
-           emp_id integer, 
-           emp_name character varying(10)
-           );
-           INSERT INTO new_emp 
-           Values (1,'Mohan'),(2,'Shyam'),(3,'Raj'),(4,'Virat'),(5,'Rohan');,
-    postgres_conn_id = "postgres_local",
-    dag = dag_psql'''
-)
-
-new = PostgresOperator(
-    task_id='new',
-    sql='''CREATE TABLE new(
+with DAG(
+    dag_id="postgres_operator_dag",
+    start_date=datetime(2023, 6, 1),
+    schedule_interval="15 11 * * *",
+    catchup=False,
+) as dag:
+    emp_details = PostgresOperator(
+        task_id="emp_details",
+        sql="""
+            CREATE TABLE new_emp(
             emp_id integer, 
             emp_name character varying(10)
             );
             INSERT INTO new_emp 
-            select * from new_emp;''',
-    postgres_conn_id="postgres_local",
-    dag=dag_psql
-)
-
-emp_details >> new
+            Values (1,'Mohan'),(2,'Shyam'),(3,'Raj'),(4,'Virat'),(5,'Rohan');
+          """,
+    )
+    new = PostgresOperator(
+        task_id='new',
+        sql='''
+            CREATE TABLE new(
+            emp_id integer, 
+            emp_name character varying(10)
+            );
+            INSERT INTO new_emp 
+            select * from new_emp;
+            '''
+    )
+    emp_details >> new
 
